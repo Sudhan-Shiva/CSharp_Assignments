3da62164c440bc1a55c0011d12013178d08e47e2
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-01-10 13:14:19 +0530

    ADD : .gitignore files

---
+++ OOP_Excercise/OOP_TASK1/Task1/DerivedClass/Circle.cs
@@ -0,0 +1,20 @@
+﻿using OOP_TASK1.Task1.BaseClass;
+
+namespace OOP_TASK1.Task1.DerivedClass
+{
+    //Define Derived class 'Circle'
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        //Constructor Block
+        public Circle(double radiusOfCircle, string colourOfCircle)
+        {
+            Radius = radiusOfCircle;
+            Colour = colourOfCircle;
+            ShapeType = "Circle";
+        }
+        //Override the CalculateArea Method
+        public override double CalculateArea() => Math.PI * Radius * Radius;
+    }
+}
