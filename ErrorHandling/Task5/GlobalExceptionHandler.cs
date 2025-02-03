e096485f540144d17fabe1ceb51c14bfac213b91
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-03 09:30:03 +0530

    feat : implement Task-5 : Global Try-Catch Block

---
+++ ErrorHandling/Task5/GlobalExceptionHandler.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Throw new exceptions which are handled in Global Try-Catch Block for any unhandled exceptions thrown.
+/// </summary>
+public class GlobalExceptionHandler
+{
+    public int ExecuteTask5(string userInput)
+    {
+        if (int.TryParse(userInput, out int result))
+        {
+            return int.Parse(userInput);
+        }
+
+        throw new Exception($"The given input '{userInput}' is in invalid Format and cannot be parsed into an integer.");
+    }
+}
---
+++ ErrorHandling/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+        try
+        {
+            globalExceptionHandler.ExecuteTask5("Invalid");
+        }
+
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+        }
+
+        Console.ReadKey();
+    }
+}
