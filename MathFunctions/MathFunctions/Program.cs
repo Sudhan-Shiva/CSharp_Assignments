a1a3b40ebfd3ff3355a76a291c8eda570ecd542f
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-06 12:48:06 +0530

    feat : create application for basic math functions

---
+++ MathFunctions/MathFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿using MathFunctions.Utility;
+
+public class Program
+{
+    static void Main()
+    {
+        MathUtils mathUtils = new MathUtils();
+
+        Console.Write("Provide the First integer : ");
+        int firstIntegerInput = int.Parse(Console.ReadLine());
+
+        Console.Write("Provide the Second integer : ");
+        int secondIntegerInput = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The result of addition between {firstIntegerInput} and {secondIntegerInput} is equal to {mathUtils.Add(firstIntegerInput, secondIntegerInput)}.");
+        Console.WriteLine($"The result of subtraction between {firstIntegerInput} and {secondIntegerInput} is equal to {mathUtils.Subtract(firstIntegerInput, secondIntegerInput)}.");
+        Console.WriteLine($"The result of multiplication between {firstIntegerInput} and {secondIntegerInput} is equal to {mathUtils.Multiply(firstIntegerInput, secondIntegerInput)}.");
+        Console.WriteLine($"The result of division between {firstIntegerInput} and {secondIntegerInput} is equal to {mathUtils.Divide(firstIntegerInput, secondIntegerInput)}.");
+
+        Console.ReadKey();
+    }
+}
---
+++ MathFunctions/MathFunctions/Utility/MathUtils.cs
@@ -0,0 +1,40 @@
+﻿namespace MathFunctions.Utility
+{
+    /// <summary>
+    /// Defines the required methods
+    /// </summary>
+    public class MathUtils
+    {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="firstInput">The first integer input</param>
+        /// <param name="secondInput">The second integer input</param>
+        /// <returns>The addition result of the two integer inputs</returns>
+        public int Add(int firstInput, int secondInput) => firstInput + secondInput;
+
+        /// <summary>
+        /// Subtracts two numbers
+        /// </summary>
+        /// <param name="firstInput">The first integer input</param>
+        /// <param name="secondInput">The second integer input</param>
+        /// <returns>The subtraction result of the two integer inputs</returns>
+        public int Subtract(int firstInput, int secondInput) => firstInput - secondInput;
+
+        /// <summary>
+        /// Multiplies two numbers
+        /// </summary>
+        /// <param name="firstInput">The first integer input</param>
+        /// <param name="secondInput">The second integer input</param>
+        /// <returns>The multiplication result of the two integer inputs</returns>
+        public int Multiply(int firstInput, int secondInput) => firstInput * secondInput;
+
+        /// <summary>
+        /// Divides two numbers
+        /// </summary>
+        /// <param name="firstInput">The first integer input</param>
+        /// <param name="secondInput">The second integer input</param>
+        /// <returns>The division result of the two integer inputs</returns>
+        public int Divide(int firstInput, int secondInput) => firstInput / secondInput;
+    }
+}
