1388ec47ebd730463373960de931e29f1f6a875a
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-07 16:41:24 +0530

    feat : implement Task-3 : test Garbage Collector

---
+++ MemoryManagement/GarbageCollection/Model/Product.cs
@@ -0,0 +1,43 @@
+﻿namespace GarbageCollection.Model
+{
+    /// <summary>
+    /// To Store the Product Information
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// The ID of the Product
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The price of the product
+        /// </summary>
+        public decimal ProductPrice { get; set; }
+
+        /// <summary>
+        /// The category of the product
+        /// </summary>
+        public string ProductCategory { get; set; }
+
+        /// <summary>
+        /// The constructor block
+        /// </summary>
+        /// <param name="productId">Id of the product</param>
+        /// <param name="productName">Name of the product</param>
+        /// <param name="productPrice">Price of the product</param>
+        /// <param name="productCategory">Category of the product</param>
+        public Product(string productName, int productId, decimal productPrice, string productCategory)
+        {
+            ProductName = productName;
+            ProductId = productId;
+            ProductPrice = productPrice;
+            ProductCategory = productCategory;
+        }
+    }
+}
---
+++ MemoryManagement/GarbageCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿using GarbageCollection.Model;
+using ConsoleTables;
+
+public class Program
+{
+    static void Main()
+    {
+        int testValue = 10;
+
+        void createArray()
+        {
+            Product[] testArray = new Product[100];
+            ConsoleTable heapSizeTable = new ConsoleTable("Loop Count","Before Creation (bytes)", "After Creation (bytes)", "After Destruction (bytes)", "Memory Freed (Bytes)");
+
+            for (int count = 0; count < 10; count++)
+            {
+                var heapSizeBeforeCreation = GC.GetTotalMemory(false);
+
+                for (int index = 0; index < testArray.Length; index++)
+                {
+                    testArray[index] = new Product($"Name {testValue++}", testValue ++, testValue --, $"Category{testValue--}");                       
+                }
+
+                Thread.Sleep(2000);
+                var heapSizeAfterCreation = GC.GetTotalMemory(false);
+
+                Thread.Sleep(2000);
+                GC.Collect();
+
+                Thread.Sleep(2000);
+                var heapSizeAfterDestruction = GC.GetTotalMemory(false);
+
+                heapSizeTable.AddRow(count,  heapSizeBeforeCreation, heapSizeAfterCreation, heapSizeAfterDestruction, heapSizeAfterCreation - heapSizeAfterDestruction);
+                Console.WriteLine("\n");
+            }
+
+            heapSizeTable.Write();
+        }
+
+        createArray(); 
+
+        Console.ReadKey();
+    }
+}
