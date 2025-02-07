f81183aa788174db81ad5c8d1d224146af4a270e
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-07 16:38:34 +0530

    feat : test value and reference types

---
+++ MemoryManagement/ValueReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using ValueReferenceTypes.Model;
+
+public class Program
+{
+    static void Main()
+    {
+        int valueTypeInteger = 1;
+        Product product = new Product("Apple", 1, 21, "Food");
+
+        Console.WriteLine("Initial Value Type :"+valueTypeInteger);
+        Console.WriteLine("Initial Reference Type :"+product.ProductName);
+
+        void ValueChange(int testValue, Product testProduct)
+        {
+            testValue++;
+            testProduct.ProductName = "Banana";
+        }
+        
+        ValueChange(valueTypeInteger, product);
+
+        Console.WriteLine("\nUpdated Value Type :" + valueTypeInteger);
+        Console.WriteLine("Updated Reference Type :" + product.ProductName);
+
+        Console.ReadKey();
+    }  
+}
