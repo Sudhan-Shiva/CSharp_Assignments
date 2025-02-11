5069058ac21bd266243f74b8b66bd7c0a99fb7c1
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-11 19:11:51 +0530

    feat : implement Project A (GreetingsApp)

---
+++ BuildOrders/GreetingsApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Pastel;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"WELCOME TO THE APPLICATION !!!".Pastel(ConsoleColor.Magenta));
+
+        Console.ReadKey();
+    }
+}
