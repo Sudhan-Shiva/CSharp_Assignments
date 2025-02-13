e1889603f774ab8048e9bcd2be17373576f46b0c
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-13 09:57:51 +0530

    feat : create text file of 1 GB size

---
+++ FilesAndStreams/FilesAndStreams/FileManager/DataHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace FilesAndStreams.FileManager
+{
+    public class DataHandler
+    {
+        public string ReadFile(string path)
+        {
+            string userDetails = File.ReadAllText(path);
+            return userDetails;
+        }
+
+        public void WriteFile(string path, string content)
+        {          
+            File.AppendAllText(path, content);
+        }
+
+        public void CreateDummyFile(string fileName, long length)
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                fileStream.SetLength(length);
+            }
+        }
+    }
+}
---
+++ FilesAndStreams/FilesAndStreams/Program.cs
@@ -0,0 +1,13 @@
+﻿using FilesAndStreams.FileManager;
+
+public class Program
+{
+    static void Main()
+    {
+        DataHandler dataHandler = new DataHandler();
+        do
+        {
+            dataHandler.WriteFile("TestFile.txt", dataHandler.ReadFile("../../../../Shakespeare.txt"));
+        } while (new FileInfo("TestFile.txt").Length < 1024 * 1024 * 1024);
+    }
+}
