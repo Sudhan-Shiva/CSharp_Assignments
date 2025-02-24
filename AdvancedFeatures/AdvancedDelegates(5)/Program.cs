7bfa41c3a6a41d2f22714e37889e78028491e847
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-24 11:07:47 +0530

    feat : implement Task-5 : advanced use of delegates

---
+++ AdvancedFeatures/AdvancedDelegates(5)/Program.cs
@@ -0,0 +1,56 @@
+﻿using AdvancedDelegates;
+using ConsoleTables;
+
+public class Program
+{
+    public delegate int SortDelegate(Product firstProduct, Product secondProduct) ;
+    static void Main()
+    {
+        List<Product> productList = new List<Product>();
+        productList.Add(new Product("Apple", "Food", 100.50));
+        productList.Add(new Product("Harry Potter", "Book", 500));
+        productList.Add(new Product("Tea", "Beverage", 15.0));
+        productList.Add(new Product("Pie", "Food", 175.20));
+        productList.Add(new Product("Origin", "Book", 370));
+
+        SortDelegate sortDelegateByName = SortByName;
+        SortDelegate sortDelegateByCategory = SortByCategory;
+        SortDelegate sortDelegateByPrice = SortByPrice;
+
+        Console.WriteLine("-----Sorting By Name------\n");
+        SortAndDisplay(sortDelegateByName, productList);
+
+        Console.WriteLine("-----Sorting By Category------\n");
+        SortAndDisplay(sortDelegateByCategory, productList);
+
+        Console.WriteLine("-----Sorting By Price------\n");
+        SortAndDisplay(sortDelegateByPrice, productList);
+
+        Console.ReadKey();
+    }
+
+    static int SortByName(Product firstProduct, Product secondProduct)
+    {
+        return firstProduct.Name.CompareTo(secondProduct.Name);
+    }
+    static int SortByCategory(Product firstProduct, Product secondProduct)
+    {
+        return firstProduct.Category.CompareTo(secondProduct.Category);
+    }
+
+    static int SortByPrice(Product firstProduct, Product secondProduct)
+    {
+        return firstProduct.Price.CompareTo(secondProduct.Price);
+    }
+
+    static void SortAndDisplay(SortDelegate sortDelegate, List<Product> productList)
+    {
+        productList.Sort(sortDelegate.Invoke);
+
+        ConsoleTable productTable = new ConsoleTable("Name", "Category", "Price");
+        foreach(Product product in productList)
+            productTable.AddRow(product.Name, product.Category, product.Price);
+
+        productTable.Write();
+    }
+}
