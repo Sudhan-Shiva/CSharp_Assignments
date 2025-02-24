90be2ac67ba884d4cad6f0d6526b7b35a19187b9
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-24 09:25:50 +0530

    feat : implement Task-3 : anonymous methods

---
+++ AdvancedFeatures/AnonymousMethods(3)/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public delegate void TestDelegate(int[] intArray);
+
+    static void Main()
+    {
+        int[] testArray = [3,1,7,2,3,5,4];
+        Console.Write("The Original Array : ");
+        foreach (int i in testArray) { Console.Write(i + ", "); }
+        Console.WriteLine();
+
+        TestDelegate anonymousMethod = delegate (int[] testArray)
+        {
+           Array.Sort(testArray);
+        };
+
+        anonymousMethod(testArray);
+
+        Console.Write("The Sorted Array : ");
+        foreach (int i in testArray) { Console.Write(i + ", "); }
+        
+        Console.ReadKey();
+    }
+}
