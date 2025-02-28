b1d266d54e30b0e3c11ff949927d04c7492fc7d2
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-28 14:30:56 +0530

    feat : implement task-3 : Dynamic Method Invoker

---
+++ Reflection/DynamicMethodInvoker(3)/MethodInvoker.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace DynamicMethodInvoker
+{
+    public class MethodInvoker
+    {
+        public void InvokeMethod(object obj, string methodName)
+        {
+            Type objectType = obj.GetType();
+            MethodInfo? method = objectType.GetMethod(methodName);
+
+            if (method != null)
+            {
+                method.Invoke(obj, null);
+                Console.WriteLine($"\nMethod : {method.Name} is successfully invoked from Type : {objectType.FullName}.");
+            }
+            else
+            {
+                Console.WriteLine($"Method {methodName} not found !!!");
+            }
+        }
+    }
+}
---
+++ Reflection/DynamicMethodInvoker(3)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+public class Program
+{
+    public static void Main()
+    {
+        string assemblyPath = "../../../../MethodHelper.dll";
+        var assembly = Assembly.LoadFrom(assemblyPath);
+        Console.WriteLine($"Assembly is successfully loaded !!!\n");
+
+        DynamicMethodInvoker.MethodInvoker methodInvoker = new DynamicMethodInvoker.MethodInvoker();
+
+        string? typeName = "Program";
+        var types = assembly.GetTypes();
+        var requiredType = types.FirstOrDefault(i => i.FullName.Equals(typeName));
+
+        string? methodName = "TestMethod";
+        methodInvoker.InvokeMethod(Activator.CreateInstance(requiredType), methodName);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
