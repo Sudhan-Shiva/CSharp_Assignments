bee143e5b81b27c0295fb79650626f7e3f32639b
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-02-28 14:29:17 +0530

    feat : implement task-2 : Dynamic Object Inspector

---
+++ Reflection/DynamicObjectInspector(2)/ObjectInspector.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Pastel;
+using ConsoleTables;
+
+namespace DynamicObjectInspector
+{
+    internal class ObjectInspector
+    {
+        public void InspectProperty(object obj)
+        {
+            Type objectType = obj.GetType();
+            Console.WriteLine($"\nType : { objectType.FullName}\n".Pastel(ConsoleColor.Magenta));
+
+            IEnumerable<PropertyInfo> objectProperties = objectType.GetProperties();
+            ConsoleTable table = new ConsoleTable("Property", "Value");
+           
+            if(objectProperties.Count() > 0)
+            {
+                foreach (PropertyInfo property in objectProperties)
+                {
+                    string propertyName = property.Name;
+                    object? propertyValue = property.GetValue(obj);
+                    table.AddRow(propertyName, propertyValue);
+                }
+                table.Write();
+            }
+            else
+                Console.WriteLine($"No properties found !!!".Pastel(ConsoleColor.Red));    
+        }
+
+        public void SetPropertyValue(object obj, string propertyName, object newValue)
+        {
+            Type objectType = obj.GetType();
+            PropertyInfo? property = objectType.GetProperty(propertyName);
+            property?.SetValue(obj, newValue);
+        }
+    }
+}
---
+++ Reflection/DynamicObjectInspector(2)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using DynamicObjectInspector;
+using Pastel;
+
+public class Program
+{
+    public static void Main()
+    {
+        string assemblyPath = "../../../../ProductClass.dll";
+        var assembly = Assembly.LoadFrom(assemblyPath);
+        var types = assembly.GetTypes();
+
+        ObjectInspector objectInspector = new ObjectInspector();
+
+        foreach (Type type in types)
+        {
+            objectInspector.InspectProperty(Activator.CreateInstance(type));
+        }
+
+        Console.WriteLine("\nAfter Modification......".Pastel(ConsoleColor.Cyan));
+
+        foreach (Type type in types)
+        {
+            object obj = Activator.CreateInstance(type);
+            objectInspector.SetPropertyValue(obj, "Name", "ModifiedName");
+            objectInspector.SetPropertyValue(obj, "Price", 100.77);
+            objectInspector.SetPropertyValue(obj, "Id", 10);
+            objectInspector.InspectProperty(obj);
+        }
+
+        Console.ReadKey();
+    }
+}
