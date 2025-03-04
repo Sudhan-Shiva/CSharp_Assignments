f6cbdd8d140c39e19041b43873d82d1c7b747bbb
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-03-04 15:58:04 +0530

    feat : implement Task-1 : AsynchronousDownloader

---
+++ AsynchronousProgramming/AsynchronousDownloader(1)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
+
+        try
+        {
+            var urlContent = await GetUrlContent(url);
+            Console.WriteLine(urlContent);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+    private static async Task<string> GetUrlContent(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
