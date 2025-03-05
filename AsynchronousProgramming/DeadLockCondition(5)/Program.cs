42f24bf3690d751976a6c1ccc6256373d7953296
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-03-05 10:50:26 +0530

    feat : task-5 : resolve deadlock by ConfigureAwait

--- AsynchronousProgramming/DeadLockCondition(5)/Program.cs
+++ AsynchronousProgramming/DeadLockCondition(5)/Program.cs
@@ -3,23 +3,24 @@
     static void Main()
     {
         DeadlockMethod();
+
         Console.ReadKey();
     }
 
     static async Task DeadlockMethod()
     {
-        var asyncResult = await SomeAsyncOperation();
-        //Here, await continues the execution asynchronously by returning the main thread to the thread pool.
+        var asyncResult = SomeAsyncOperation().Result;
+        //Result blocks main thread from execution till the asyncResult is returned from the SomeAsyncOperation().
 
         Console.WriteLine(asyncResult);
     }
 
     static async Task<string> SomeAsyncOperation()
     {
-        await Task.Delay(2000);
-        //Here, await continues the execution on the main thread asynchronously as the delay occurs.
-        //The execution is attempted to continue on the main thread.
-        //This doesn't cause deadlock since the main thread is available for execution since await is an non-blocking operation.
+        await Task.Delay(1000).ConfigureAwait(false);
+        //Here, await continues the execution asynchronously as the delay occurs.
+        //The execution is attempted to continue on a different context since the ConfigureAwait(false) is used.
+        //This resolves any deadlock situation since the program executes on a different context.
 
         return "Hello, World!";
     }
