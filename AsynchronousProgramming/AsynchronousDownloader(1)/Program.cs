54025e69424af8439b81b0e4d7918957e67e9b67
Sudhan Shiva <sudhan.shiva@solitontech.local>
2025-03-05 15:29:16 +0530

    feat : implement Task-7 : Exception Handling

--- AsynchronousProgramming/AsynchronousDownloader(1)/Program.cs
+++ AsynchronousProgramming/AsynchronousDownloader(1)/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-
-class Program
+﻿public class Program
 {
     static async Task Main(string[] args)
     {
---
+++ AsynchronousProgramming/AsynchronousExceptions(7)/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        TestAsynchronousExceptions();
+        Console.ReadKey();
+    }
+
+    async static void ThrowAsyncVoidException()
+    {
+        //throw new Exception("This is an exception thrown from an async void method !!!");
+        //The async void methods throws exception on the synchronisation context and can only be handled by the AppDomain.Unhandled exception.
+    }
+
+    async static Task ThrowAsyncTaskException()
+    {
+        throw new Exception("This is an exception thrown from an async task method !!!");
+        //The async task method throws exception along with the task returned and therefore the exception can be caught using a try-catch block.
+    }
+
+    async static void TestAsynchronousExceptions()
+    {
+        try
+        {
+            ThrowAsyncVoidException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        try
+        {
+            await ThrowAsyncTaskException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
